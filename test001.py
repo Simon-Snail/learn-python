ef9ab8bf3935c90f2076700fef902e6ed2d73bb3
liujin <Lj1234567890>
2020-04-27 17:24:02 +0800

    commit

--- test001.py
+++ test001.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from pojo import Student
 # print('中文测试正常')
 #name = input("please enter you name: ")
 #print("Hello, ", name)
@@ -26,3 +27,50 @@
 # l = os.listdir('.')
 # print(l)
 # 导入turtle包的所有内容:
+# import os
+# print([x*x for x in range(1,10) if x%2 == 0])
+
+# l = [x + n for x in 'ABC' for n in 'CDF']
+# print(l)
+# print([d for d in os.listdir()])
+# L = ['Hello', 'World', 18, 'Apple', None]
+# g = (x.lower() for x in L if isinstance(x, str))
+# print(next(g))
+# from datetime import datetime
+# now = datetime.now()
+# print(now)
+
+
+def add(x, y, f):
+    return f(x) + f(y)
+
+
+L1 = ['adam', 'LISA', 'barT']
+
+
+def normalize(name):
+    return name.title()
+
+
+python = 'python'
+#####    输出   #####
+words = ['python', 'java', 'c']
+print("""
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+i = 5
+i = 6
+def f(arg=i):
+    print(arg)
+
+
+f()
+
+from collections import deque
+t = 12345, 54321, 'hello!'
+a, b, c = t
+print(a, b, c)
+
